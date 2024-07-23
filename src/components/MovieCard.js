481cff79fc0512af0a095679f9e5549a5762a181
nicolasmatte2 <nickmtt21@gmail.com>
2024-07-23 00:49:23 -0300

    another changes in package.json for github page

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -19,7 +19,7 @@ export default function MovieCard({ movie, isWatchlisted, toggleWatchlist }) {
   return (
     <div key={movie.id} className="movie-card">
       <img
-        src={`images/${movie.image}`}
+        src={`${process.env.PUBLIC_URL}/images/${movie.image}`}
         alt={movie.title}
         onError={handleError}
       />
